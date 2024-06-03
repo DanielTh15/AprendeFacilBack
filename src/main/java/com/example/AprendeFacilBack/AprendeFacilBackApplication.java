407313961d101d8ab22c961aa3f0dd2d936883b2
Daniel_Thevening <gonfidaniel@gmail.com>
2024-06-03 13:47:25 -0500

    first commit

---
+++ src/main/java/com/example/AprendeFacilBack/AprendeFacilBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.AprendeFacilBack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AprendeFacilBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AprendeFacilBackApplication.class, args);
+	}
+
+}
